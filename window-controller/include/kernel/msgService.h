36e29a8385bc9348de37db16cd13145a6493578e
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2025-01-26 18:16:59 +0100

    window: add msgService class

---
+++ window-controller/include/kernel/msgService.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <Arduino.h>
+
+/* ======= Message class ======= */
+class Msg {
+  String content;
+
+public:
+  Msg(String content){
+    this->content = content;
+  }
+  
+  String getContent() const {
+    return content;
+  }
+};
+
+class Pattern {
+public:
+  virtual boolean match(const Msg& m) = 0;  
+};
+
+/* ======= Messages Handler ======= */
+class MsgServiceClass {
+    
+public: 
+  
+  Msg* currentMsg;
+  bool msgAvailable;
+
+  void init();  
+
+  bool isMsgAvailable();
+  Msg* receiveMsg();
+
+  bool isMsgAvailable(Pattern& pattern);
+
+  /* note: message deallocation is responsibility of the client */
+  Msg* receiveMsg(Pattern& pattern);
+  
+  void sendMsg(const String& msg);
+};
+
+extern MsgServiceClass MsgService;
